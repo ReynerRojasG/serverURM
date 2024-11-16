a6a4131ac4d25ea1319ce643beb0e181e43e595f
ReynerRojasG <142065130+ReynerRojasG@users.noreply.github.com>
2024-11-15 22:15:16 -0600

    telegram_fixed

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from app import create_app
-from app.services.telegram_service import start_bot 
+from app.services.telegram_service import start_bot
 import threading
 
 app = create_app()
 
-if __name__ == "__main__":
-    #bot_thread = threading.Thread(target=start_bot)
-    #bot_thread.start()
+def run_flask():
     app.run(debug=True, use_reloader=False)
 
-    
+if __name__ == "__main__":
+    bot_thread = threading.Thread(target=start_bot, args=(app,), daemon=True)
+    bot_thread.start()
+    run_flask()
